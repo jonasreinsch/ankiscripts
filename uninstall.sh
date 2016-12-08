00f4ac353f9625204a82867f071308d1560d69cc
Jonas Reinsch <jonas.reinsch@gmail.com>
2016-12-08 16:57:35 +0100

    add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $# != 1 ]]; then
+   echo "Usage: $0 YOUR_BIN_DIR"
+   exit 1
+fi
+
+bin="$(realpath $1)"
+here="$(dirname $(realpath $0))"
+
+if [[ ! -e "$bin" ]]; then
+    echo "Error: ${bin} does not exist."
+    exit 1
+fi
+
+if [[ ! -d "$bin" ]]; then
+    echo "Error: ${bin} is no directory."
+    exit 1
+fi
+
+if [[ ! -w "$bin" ]]; then
+    echo "Error: ${bin} is not writable."
+    exit 1
+fi
+
+script_list=(
+ankicount
+ankidoublecolon    
+)
+
+for s in "${script_list[@]}"; do
+    rm "${bin}/${s}"
+done
